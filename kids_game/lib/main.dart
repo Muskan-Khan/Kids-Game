5ff7689823f4645b65a686e18d7d7fc9dc08e8d2
Jawed Alam <zjaweds@outlook.com>
2021-07-07 14:31:43 +0530

    Added broken code for cloud

--- kids_game/lib/main.dart
+++ kids_game/lib/main.dart
@@ -27,26 +27,41 @@ class Background extends StatelessWidget {
 }
 
 class ShadeOfCloud extends CustomClipper<Path> {
+  Paint _paint = Paint()
+    ..color = Colors.black
+    ..strokeWidth = 4.0
+    ..style = PaintingStyle.stroke
+    ..strokeJoin = StrokeJoin.round;
+
   @override
-  Path getClip(Size size) {
+  Path getClip(Canvas canvas, Size size) {
     Path path = new Path();
     path.moveTo(0, size.height / 4);
+    canvas.drawPath(path, _paint);
     path.quadraticBezierTo(
         size.width / 5.5, size.height / 3.5, size.width / 4, size.height / 5);
     path.quadraticBezierTo(
         size.width / 3, size.height / 4.4, size.width / 2.6, size.height / 7);
+    canvas.drawPath(path, _paint);
+
     path.close();
     path.moveTo(size.width / 2.6, size.height / 7);
     path.quadraticBezierTo(size.width / 1.9, size.height / 4.9,
         size.width / 1.7, size.height / 9.5);
+    canvas.drawPath(path, _paint);
+
     path.close();
     path.moveTo(size.width / 1.7, size.height / 9.5);
     path.quadraticBezierTo(
         size.width / 1.5, size.height / 6, size.width / 1.2, size.height / 9.4);
+    canvas.drawPath(path, _paint);
+
     path.close();
     path.moveTo(size.width / 1.2, size.height / 9.4);
     path.quadraticBezierTo(
         size.width / 1.2, size.height / 4.4, size.width, size.height / 4);
+    canvas.drawPath(path, _paint);
+
     path.lineTo(size.width, 0);
     path.lineTo(0, 0);
     path.lineTo(0, size.height / 4);
