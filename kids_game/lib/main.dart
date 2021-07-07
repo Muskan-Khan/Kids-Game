2acf5274d98526d3783701607c56abf97eba12df
Jawed Alam <zjaweds@outlook.com>
2021-07-07 13:56:58 +0530

    Cloud without border added after commenting the earlier code for Game Segments

--- kids_game/lib/main.dart
+++ kids_game/lib/main.dart
@@ -1,22 +1,78 @@
 import 'package:flutter/material.dart';
-import 'HomeScreen/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(Background());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class Background extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: 'Background',
+      home: Scaffold(
+        appBar: AppBar(
+          // title: Text('Background'),
+          toolbarHeight: 1,
+        ),
+        body: ClipPath(
+          clipper: ShadeOfCloud(),
+          child: Container(
+            color: Color(0xffE6FCFF),
+            // height: 118.0,
+          ),
+        ),
       ),
-      home: HomeScreen(),
     );
   }
 }
+
+class ShadeOfCloud extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = new Path();
+    path.moveTo(0, size.height / 4);
+    path.quadraticBezierTo(
+        size.width / 5.5, size.height / 3.5, size.width / 4, size.height / 5);
+    path.quadraticBezierTo(
+        size.width / 3, size.height / 4.4, size.width / 2.6, size.height / 7);
+    path.close();
+    path.moveTo(size.width / 2.6, size.height / 7);
+    path.quadraticBezierTo(size.width / 1.9, size.height / 4.9,
+        size.width / 1.7, size.height / 9.5);
+    path.close();
+    path.moveTo(size.width / 1.7, size.height / 9.5);
+    path.quadraticBezierTo(
+        size.width / 1.5, size.height / 6, size.width / 1.2, size.height / 9.4);
+    path.close();
+    path.moveTo(size.width / 1.2, size.height / 9.4);
+    path.quadraticBezierTo(
+        size.width / 1.2, size.height / 4.4, size.width, size.height / 4);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+    path.lineTo(0, size.height / 4);
+    path.lineTo(size.width / 2.6, size.height / 7);
+    path.lineTo(size.width / 1.7, size.height / 9.5);
+    path.lineTo(size.width / 1.2, size.height / 9.4);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
+
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: HomeScreen(),
+//     );
+//   }
+// }
