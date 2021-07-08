a7869e6f334293f8af0fbcf745f61f23c07b4027
Jawed Alam <zjaweds@outlook.com>
2021-07-08 21:29:36 +0530

    Files that have potential to create chaos

---
+++ kids_game/lib/HomeScreen/background.dart
@@ -0,0 +1,69 @@
+// import 'package:flutter/material.dart';
+// // import 'HomeScreen/home_screen.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Background());
+}
+
+class Background extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Background',
+      home: Scaffold(
+        appBar: AppBar(
+          // title: Text('Background'),
+          toolbarHeight: 1,
+        ),
+        body: ClipPath(
+          clipper: ShadeOfCloud(),
+          child: Container(
+            color: Color(0xff14C7DF),
+            // height: 118.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ShadeOfCloud extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = new Path();
+    path.moveTo(0, size.height / 4);
+    path.quadraticBezierTo(
+        size.width / 5.5, size.height / 3.5, size.width / 4, size.height / 5);
+    path.quadraticBezierTo(
+        size.width / 3, size.height / 4.4, size.width / 2.6, size.height / 7);
+    path.close();
+    path.moveTo(size.width / 2.6, size.height / 7);
+    path.quadraticBezierTo(size.width / 1.9, size.height / 4.9,
+        size.width / 1.7, size.height / 9.5);
+
+    path.close();
+    path.moveTo(size.width / 1.7, size.height / 9.5);
+    path.quadraticBezierTo(
+        size.width / 1.5, size.height / 6, size.width / 1.2, size.height / 9.4);
+    path.close();
+    path.moveTo(size.width / 1.2, size.height / 9.4);
+    path.quadraticBezierTo(
+        size.width / 1.2, size.height / 4.4, size.width, size.height / 4);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+    path.lineTo(0, size.height / 4);
+    path.lineTo(size.width / 2.6, size.height / 7);
+    path.lineTo(size.width / 1.7, size.height / 9.5);
+    path.lineTo(size.width / 1.2, size.height / 9.4);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
--- kids_game/lib/main.dart
+++ kids_game/lib/main.dart
@@ -1,93 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(Background());
-}
+void main() => runApp(CountingScreen());
 
-class Background extends StatelessWidget {
+class CountingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Background',
       home: Scaffold(
-        appBar: AppBar(
-          // title: Text('Background'),
-          toolbarHeight: 1,
-        ),
-        body: ClipPath(
-          clipper: ShadeOfCloud(),
-          child: Container(
-            color: Color(0xffE6FCFF),
-            // height: 118.0,
-          ),
-        ),
+        body: InstanceOfGame(),
       ),
     );
   }
 }
 
-class ShadeOfCloud extends CustomClipper<Path> {
-  Paint _paint = Paint()
-    ..color = Colors.black
-    ..strokeWidth = 4.0
-    ..style = PaintingStyle.stroke
-    ..strokeJoin = StrokeJoin.round;
-
-  @override
-  Path getClip(Canvas canvas, Size size) {
-    Path path = new Path();
-    path.moveTo(0, size.height / 4);
-    canvas.drawPath(path, _paint);
-    path.quadraticBezierTo(
-        size.width / 5.5, size.height / 3.5, size.width / 4, size.height / 5);
-    path.quadraticBezierTo(
-        size.width / 3, size.height / 4.4, size.width / 2.6, size.height / 7);
-    canvas.drawPath(path, _paint);
-
-    path.close();
-    path.moveTo(size.width / 2.6, size.height / 7);
-    path.quadraticBezierTo(size.width / 1.9, size.height / 4.9,
-        size.width / 1.7, size.height / 9.5);
-    canvas.drawPath(path, _paint);
-
-    path.close();
-    path.moveTo(size.width / 1.7, size.height / 9.5);
-    path.quadraticBezierTo(
-        size.width / 1.5, size.height / 6, size.width / 1.2, size.height / 9.4);
-    canvas.drawPath(path, _paint);
-
-    path.close();
-    path.moveTo(size.width / 1.2, size.height / 9.4);
-    path.quadraticBezierTo(
-        size.width / 1.2, size.height / 4.4, size.width, size.height / 4);
-    canvas.drawPath(path, _paint);
-
-    path.lineTo(size.width, 0);
-    path.lineTo(0, 0);
-    path.lineTo(0, size.height / 4);
-    path.lineTo(size.width / 2.6, size.height / 7);
-    path.lineTo(size.width / 1.7, size.height / 9.5);
-    path.lineTo(size.width / 1.2, size.height / 9.4);
-    return path;
+class InstanceOfGame extends StatelessWidget {
+  final int n = 5;
+  Alpha() {
+    for (int i = 0; i < 1; i++) {
+      Text(n.toString());
+    }
   }
 
   @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[Alpha()],
+    );
+  }
 }
-
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: HomeScreen(),
-//     );
-//   }
-// }
