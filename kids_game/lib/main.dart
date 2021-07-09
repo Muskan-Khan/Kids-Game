0621f6d32fb9a4832da954e886bd6685a07b5908
Baby Muskan <babymuskan002@gmail.com>
2021-07-10 00:30:20 +0530

    Audio files added

--- kids_game/lib/main.dart
+++ kids_game/lib/main.dart
@@ -1,156 +1,157 @@
 import 'package:flutter/material.dart';
+import 'Games/counting_screen.dart';
 
 void main() => runApp(CountingScreen());
 
-class CountingScreen extends StatefulWidget {
-  const CountingScreen({Key? key}) : super(key: key);
+// class CountingScreen extends StatefulWidget {
+//   const CountingScreen({Key? key}) : super(key: key);
 
-  @override
-  _CountingScreenState createState() => _CountingScreenState();
-}
+//   @override
+//   _CountingScreenState createState() => _CountingScreenState();
+// }
 
-class _CountingScreenState extends State<CountingScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      textDirection: TextDirection.ltr,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.center,
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        InstanceOfGame(
-          numberOfCards: 6,
-        ),
-      ],
-    );
-  }
-}
+// class _CountingScreenState extends State<CountingScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       textDirection: TextDirection.ltr,
+//       crossAxisAlignment: CrossAxisAlignment.start,
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       mainAxisSize: MainAxisSize.max,
+//       children: [
+//         InstanceOfGame(
+//           numberOfCards: 6,
+//         ),
+//       ],
+//     );
+//   }
+// }
 
-class InstanceOfGame extends StatelessWidget {
-  final List<Image> imgs = [
-    Image.asset('assets/images/bird.png'),
-    Image.asset('assets/images/monkey.png'),
-    Image.asset('assets/images/owls.png')
-  ];
+// class InstanceOfGame extends StatelessWidget {
+//   final List<Image> imgs = [
+//     Image.asset('assets/images/bird.png'),
+//     Image.asset('assets/images/monkey.png'),
+//     Image.asset('assets/images/owls.png')
+//   ];
 
-  final int numberOfCards;
-  InstanceOfGame({Key? key, required this.numberOfCards}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.max,
-      verticalDirection: VerticalDirection.down,
-      textDirection: TextDirection.ltr,
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: <Widget>[
-        for (var i = 0; i < numberOfCards; i++)
-          Alpha(
-            img: imgs[1],
-          ),
-        Options(numberOfCards),
-      ],
-    );
-  }
-}
+//   final int numberOfCards;
+//   InstanceOfGame({Key? key, required this.numberOfCards}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       mainAxisSize: MainAxisSize.max,
+//       verticalDirection: VerticalDirection.down,
+//       textDirection: TextDirection.ltr,
+//       mainAxisAlignment: MainAxisAlignment.start,
+//       children: <Widget>[
+//         for (var i = 0; i < numberOfCards; i++)
+//           Alpha(
+//             img: imgs[1],
+//           ),
+//         Options(numberOfCards),
+//       ],
+//     );
+//   }
+// }
 
-class Alpha extends StatelessWidget {
-  final Image img;
-  Alpha({Key? key, required this.img}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return img;
-  }
-}
+// class Alpha extends StatelessWidget {
+//   final Image img;
+//   Alpha({Key? key, required this.img}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return img;
+//   }
+// }
 
-class Options extends StatelessWidget {
-  final int rightAnswer;
-  Options(this.rightAnswer);
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        textDirection: TextDirection.ltr,
-        verticalDirection: VerticalDirection.down,
-        textBaseline: TextBaseline.alphabetic,
-        children: [
-          Card(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(0),
-                side: BorderSide(
-                  color: Colors.black,
-                )),
-            child: Center(
-              child: Container(
-                height: 80,
-                width: 80,
-                color: Colors.blue,
-                padding: EdgeInsets.all(2.0),
-                child: Text(
-                  (rightAnswer * 2).toString(),
-                  textDirection: TextDirection.ltr,
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-          ),
-          Card(
-            child: Center(
-              child: Container(
-                height: 80,
-                width: 80,
-                color: Colors.blue,
-                padding: EdgeInsets.all(2.0),
-                child: Text(
-                  (rightAnswer + 2).toString(),
-                  textDirection: TextDirection.ltr,
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-          ),
-          Card(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(0),
-                side: BorderSide(
-                  color: Colors.black,
-                )),
-            child: Center(
-              child: Container(
-                height: 80,
-                width: 80,
-                color: Colors.blue,
-                padding: EdgeInsets.all(2.0),
-                child: Text(
-                  (rightAnswer * 1.5).toString(),
-                  textDirection: TextDirection.ltr,
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-          ),
-          Card(
-            child: Center(
-              child: Container(
-                height: 80,
-                width: 80,
-                color: Colors.blue,
-                padding: EdgeInsets.all(2.0),
-                child: Text(
-                  (rightAnswer + 3).toString(),
-                  textDirection: TextDirection.ltr,
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+// class Options extends StatelessWidget {
+//   final int _rightAnswer;
+//   Options(this._rightAnswer);
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: Row(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         mainAxisSize: MainAxisSize.max,
+//         crossAxisAlignment: CrossAxisAlignment.center,
+//         textDirection: TextDirection.ltr,
+//         verticalDirection: VerticalDirection.down,
+//         textBaseline: TextBaseline.alphabetic,
+//         children: [
+//           Card(
+//             shape: RoundedRectangleBorder(
+//                 borderRadius: BorderRadius.circular(0),
+//                 side: BorderSide(
+//                   color: Colors.black,
+//                 )),
+//             child: Center(
+//               child: Container(
+//                 height: 80,
+//                 width: 80,
+//                 color: Colors.blue,
+//                 padding: EdgeInsets.all(2.0),
+//                 child: Text(
+//                   (_rightAnswer * 2).toString(),
+//                   textDirection: TextDirection.ltr,
+//                   textAlign: TextAlign.center,
+//                 ),
+//               ),
+//             ),
+//           ),
+//           Card(
+//             child: Center(
+//               child: Container(
+//                 height: 80,
+//                 width: 80,
+//                 color: Colors.blue,
+//                 padding: EdgeInsets.all(2.0),
+//                 child: Text(
+//                   (_rightAnswer + 2).toString(),
+//                   textDirection: TextDirection.ltr,
+//                   textAlign: TextAlign.center,
+//                 ),
+//               ),
+//             ),
+//           ),
+//           Card(
+//             shape: RoundedRectangleBorder(
+//                 borderRadius: BorderRadius.circular(0),
+//                 side: BorderSide(
+//                   color: Colors.black,
+//                 )),
+//             child: Center(
+//               child: Container(
+//                 height: 80,
+//                 width: 80,
+//                 color: Colors.blue,
+//                 padding: EdgeInsets.all(2.0),
+//                 child: Text(
+//                   (_rightAnswer * 1.5).toString(),
+//                   textDirection: TextDirection.ltr,
+//                   textAlign: TextAlign.center,
+//                 ),
+//               ),
+//             ),
+//           ),
+//           Card(
+//             child: Center(
+//               child: Container(
+//                 height: 80,
+//                 width: 80,
+//                 color: Colors.blue,
+//                 padding: EdgeInsets.all(2.0),
+//                 child: Text(
+//                   (_rightAnswer + 3).toString(),
+//                   textDirection: TextDirection.ltr,
+//                   textAlign: TextAlign.center,
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
 
 
 
