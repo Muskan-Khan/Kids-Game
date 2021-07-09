7bf679e07646c153253636d85f58b115df4586be
Jawed Alam <zjaweds@outlook.com>
2021-07-09 14:48:54 +0530

    Dynamic rendering of images using for loop

--- kids_game/lib/main.dart
+++ kids_game/lib/main.dart
@@ -2,29 +2,52 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(CountingScreen());
 
-class CountingScreen extends StatelessWidget {
+class CountingScreen extends StatefulWidget {
+  const CountingScreen({Key? key}) : super(key: key);
+
+  @override
+  _CountingScreenState createState() => _CountingScreenState();
+}
+
+class _CountingScreenState extends State<CountingScreen> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: InstanceOfGame(),
+    return Container(
+      child: InstanceOfGame(
+        numberOfCards: 3,
       ),
     );
   }
 }
 
 class InstanceOfGame extends StatelessWidget {
-  final int n = 5;
-  Alpha() {
-    for (int i = 0; i < 1; i++) {
-      Text(n.toString());
-    }
-  }
+  final List<Image> imgs = [
+    Image.asset('assets/images/bird.png'),
+    Image.asset('assets/images/monkey.png'),
+    Image.asset('assets/images/owls.png')
+  ];
 
+  // imgs.add();
+  final int numberOfCards;
+  InstanceOfGame({Key? key, required this.numberOfCards}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: <Widget>[Alpha()],
+      children: <Widget>[
+        for (var i = 0; i < numberOfCards; i++)
+          Alpha(
+            img: imgs[1],
+          ),
+      ],
     );
+  }
+}
+
+class Alpha extends StatelessWidget {
+  final Image img;
+  Alpha({Key? key, required this.img}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return img;
   }
 }
