83b9a809fe0d49aec265f2ecf3a48d06617251f0
Jawed Alam <zjaweds@outlook.com>
2021-07-10 10:10:19 +0530

    Videoplayer added

--- kids_game/lib/main.dart
+++ kids_game/lib/main.dart
@@ -1,172 +1,105 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-void main() => runApp(CountingScreen());
+void main() => runApp(VideoPlayerApp());
 
-class CountingScreen extends StatefulWidget {
-  const CountingScreen({Key? key}) : super(key: key);
-
-  @override
-  _CountingScreenState createState() => _CountingScreenState();
-}
-
-class _CountingScreenState extends State<CountingScreen> {
+class VideoPlayerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: InstanceOfGame(
-      numberOfCards: 4,
-    ));
-  }
-}
-
-class InstanceOfGame extends StatelessWidget {
-  final List<Image> imgs = [
-    Image.asset('assets/images/bird.png'),
-    Image.asset('assets/images/monkey.png'),
-    Image.asset('assets/images/owls.png')
-  ];
-
-  final int numberOfCards;
-  InstanceOfGame({Key? key, required this.numberOfCards}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.max,
-      verticalDirection: VerticalDirection.down,
-      textDirection: TextDirection.ltr,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        for (var i = 0; i < numberOfCards; i++)
-          Flexible(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.only(left: 100 * (i % 2).toDouble()),
-              child: Alpha(
-                img: imgs[1],
-              ),
-            ),
-          ),
-        Options(4)
-      ],
+    return MaterialApp(
+      title: 'Video Player Demo',
+      home: VideoPlayerScreen(),
     );
   }
 }
 
-class Alpha extends StatelessWidget {
-  final Image img;
-  Alpha({Key? key, required this.img}) : super(key: key);
+class VideoPlayerScreen extends StatefulWidget {
+  VideoPlayerScreen({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
-    return img;
-  }
+  _VideoPlayerScreenState createState() => _VideoPlayerScreenState();
 }
 
-class Options extends StatelessWidget {
-  final int rightAnswer;
-  Options(this.rightAnswer);
+class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
+  late VideoPlayerController _controller;
+  late Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    // Create and store the VideoPlayerController. The VideoPlayerController
+    // offers several different constructors to play videos from assets, files,
+    // or the internet.
+    _controller = VideoPlayerController.network(
+      'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+    );
+
+    // Initialize the controller and store the Future for later use.
+    _initializeVideoPlayerFuture = _controller.initialize();
+
+    // Use the controller to loop the video.
+    _controller.setLooping(true);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // Ensure disposing of the VideoPlayerController to free up resources.
+    _controller.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        textDirection: TextDirection.ltr,
-        verticalDirection: VerticalDirection.down,
-        textBaseline: TextBaseline.alphabetic,
-        children: [
-          Card(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(0),
-                side: BorderSide(
-                  color: Colors.black,
-                )),
-            child: Center(
-              child: Container(
-                  height: 80,
-                  width: 80,
-                  color: Colors.blue,
-                  padding: EdgeInsets.all(2.0),
-                  child: Center(
-                    child: Text(
-                      (rightAnswer * 2).toString(),
-                      textDirection: TextDirection.ltr,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 50),
-                    ),
-                  )),
-            ),
-          ),
-          Card(
-            child: Center(
-                child: Container(
-                    height: 80,
-                    width: 80,
-                    color: Colors.blue,
-                    padding: EdgeInsets.all(2.0),
-                    child: Center(
-                      child: Text(
-                        (rightAnswer + 2).toString(),
-                        textDirection: TextDirection.ltr,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 50),
-                      ),
-                    ))),
-          ),
-          Card(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(0),
-                side: BorderSide(
-                  color: Colors.black,
-                )),
-            child: Center(
-              child: Container(
-                  height: 80,
-                  width: 80,
-                  color: Colors.blue,
-                  padding: EdgeInsets.all(2.0),
-                  child: Center(
-                    child: Text(
-                      (rightAnswer * 1.5).toString(),
-                      textDirection: TextDirection.ltr,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 50),
-                    ),
-                  )),
-            ),
-          ),
-          Card(
-            child: Center(
-              child: Container(
-                  height: 80,
-                  width: 80,
-                  color: Colors.blue,
-                  padding: EdgeInsets.all(2.0),
-                  child: Center(
-                    child: Text(
-                      (rightAnswer + 3).toString(),
-                      textDirection: TextDirection.ltr,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 50),
-                    ),
-                  )),
-            ),
-          ),
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Butterfly Video'),
+      ),
+      // Use a FutureBuilder to display a loading spinner while waiting for the
+      // VideoPlayerController to finish initializing.
+      body: FutureBuilder(
+        future: _initializeVideoPlayerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If the VideoPlayerController has finished initialization, use
+            // the data it provides to limit the aspect ratio of the video.
+            return AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              // Use the VideoPlayer widget to display the video.
+              child: VideoPlayer(_controller),
+            );
+          } else {
+            // If the VideoPlayerController is still initializing, show a
+            // loading spinner.
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Wrap the play or pause in a call to `setState`. This ensures the
+          // correct icon is shown.
+          setState(() {
+            // If the video is playing, pause it.
+            if (_controller.value.isPlaying) {
+              _controller.pause();
+            } else {
+              // If the video is paused, play it.
+              _controller.play();
+            }
+          });
+        },
+        // Display the correct icon depending on the state of the player.
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -179,6 +112,7 @@ class Options extends StatelessWidget {
 
 // class CountingScreen extends StatefulWidget {
 //   const CountingScreen({Key? key}) : super(key: key);
+
 //   @override
 //   _CountingScreenState createState() => _CountingScreenState();
 // }
@@ -186,16 +120,9 @@ class Options extends StatelessWidget {
 // class _CountingScreenState extends State<CountingScreen> {
 //   @override
 //   Widget build(BuildContext context) {
-//     return Container(
-//         child: Column(
-//       children: [
-//         InstanceOfGame(
-//           numberOfCards: 3,
-//         ),
-//         InstanceOfGame(
-//           numberOfCards: 2,
-//         ),
-//       ],
+//     return Center(
+//         child: InstanceOfGame(
+//       numberOfCards: 4,
 //     ));
 //   }
 // }
@@ -206,19 +133,28 @@ class Options extends StatelessWidget {
 //     Image.asset('assets/images/monkey.png'),
 //     Image.asset('assets/images/owls.png')
 //   ];
-//   // imgs.add();
+
 //   final int numberOfCards;
 //   InstanceOfGame({Key? key, required this.numberOfCards}) : super(key: key);
 //   @override
 //   Widget build(BuildContext context) {
 //     return Column(
-//       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//       mainAxisSize: MainAxisSize.max,
+//       verticalDirection: VerticalDirection.down,
+//       textDirection: TextDirection.ltr,
+//       mainAxisAlignment: MainAxisAlignment.center,
 //       children: <Widget>[
 //         for (var i = 0; i < numberOfCards; i++)
-//           Alpha(
-//             img: imgs[1],
+//           Flexible(
+//             flex: 1,
+//             child: Container(
+//               padding: EdgeInsets.only(left: 100 * (i % 2).toDouble()),
+//               child: Alpha(
+//                 img: imgs[1],
+//               ),
+//             ),
 //           ),
-//         Options(rightAnswer: numberOfCards)
+//         Options(numberOfCards)
 //       ],
 //     );
 //   }
@@ -235,29 +171,97 @@ class Options extends StatelessWidget {
 
 // class Options extends StatelessWidget {
 //   final int rightAnswer;
-//   const Options({Key? key, required this.rightAnswer}) : super(key: key);
+//   Options(this.rightAnswer);
 //   @override
 //   Widget build(BuildContext context) {
-//     return Row(
-      // mainAxisAlignment: MainAxisAlignment.start,
-      // mainAxisSize: MainAxisSize.max,
-      // crossAxisAlignment: CrossAxisAlignment.center,
-      // textDirection: TextDirection.ltr,
-      // verticalDirection: VerticalDirection.down,
-      // textBaseline: TextBaseline.alphabetic,
-//       // ignore: prefer_const_literals_to_create_immutables
-//       children: [
-//         const Card(
-//           color: Colors.red,
-//           shape: RoundedRectangleBorder(),
-//           child: Center(
-//             child: Text(
-//               "Right Answer",
-//               textDirection: TextDirection.ltr,
+//     return Container(
+//       child: Row(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         mainAxisSize: MainAxisSize.max,
+//         crossAxisAlignment: CrossAxisAlignment.center,
+//         textDirection: TextDirection.ltr,
+//         verticalDirection: VerticalDirection.down,
+//         textBaseline: TextBaseline.alphabetic,
+//         children: [
+//           Card(
+//             shape: RoundedRectangleBorder(
+//                 borderRadius: BorderRadius.circular(0),
+//                 side: BorderSide(
+//                   color: Colors.black,
+//                 )),
+//             child: Center(
+//               child: Container(
+//                   height: 80,
+//                   width: 80,
+//                   color: Colors.blue,
+//                   padding: EdgeInsets.all(2.0),
+//                   child: Center(
+//                     child: Text(
+//                       (rightAnswer * 2).toString(),
+//                       textDirection: TextDirection.ltr,
+//                       textAlign: TextAlign.center,
+//                       style: TextStyle(fontSize: 50),
+//                     ),
+//                   )),
 //             ),
 //           ),
-//         )
-//       ],
+//           Card(
+//             child: Center(
+//                 child: Container(
+//                     height: 80,
+//                     width: 80,
+//                     color: Colors.blue,
+//                     padding: EdgeInsets.all(2.0),
+//                     child: Center(
+//                       child: Text(
+//                         (rightAnswer + 2).toString(),
+//                         textDirection: TextDirection.ltr,
+//                         textAlign: TextAlign.center,
+//                         style: TextStyle(fontSize: 50),
+//                       ),
+//                     ))),
+//           ),
+//           Card(
+//             shape: RoundedRectangleBorder(
+//                 borderRadius: BorderRadius.circular(0),
+//                 side: BorderSide(
+//                   color: Colors.black,
+//                 )),
+//             child: Center(
+//               child: Container(
+//                   height: 80,
+//                   width: 80,
+//                   color: Colors.blue,
+//                   padding: EdgeInsets.all(2.0),
+//                   child: Center(
+//                     child: Text(
+//                       (rightAnswer).toString(),
+//                       textDirection: TextDirection.ltr,
+//                       textAlign: TextAlign.center,
+//                       style: TextStyle(fontSize: 50),
+//                     ),
+//                   )),
+//             ),
+//           ),
+//           Card(
+//             child: Center(
+//               child: Container(
+//                   height: 80,
+//                   width: 80,
+//                   color: Colors.blue,
+//                   padding: EdgeInsets.all(2.0),
+//                   child: Center(
+//                     child: Text(
+//                       (rightAnswer + 3).toString(),
+//                       textDirection: TextDirection.ltr,
+//                       textAlign: TextAlign.center,
+//                       style: TextStyle(fontSize: 50),
+//                     ),
+//                   )),
+//             ),
+//           ),
+//         ],
+//       ),
 //     );
 //   }
 // }
