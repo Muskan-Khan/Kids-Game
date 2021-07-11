1968114aeb569b12a1fa81ad6680b6479f743005
Jawed Alam <zjaweds@outlook.com>
2021-07-11 14:40:33 +0530

    Added onTap method just for the purpose of test if the cards in any lower layer are working or not

--- kids_game/lib/HomeScreen/display_card.dart
+++ kids_game/lib/HomeScreen/display_card.dart
@@ -9,32 +9,39 @@ class DisplayCard extends StatelessWidget {
     // final screen_width = screen_size;
     final screen_height = MediaQuery.of(context).size.height;
 
-    return Card(
-      elevation: 50,
-      shape: RoundedRectangleBorder(
-        side: BorderSide(
-          color: Colors.black,
-          width: screen_width / 150,
-        ),
-        borderRadius: BorderRadius.circular(50),
-      ),
-      child: SizedBox(
-          width: screen_width / 2.8,
-          height: screen_height / 5.9,
-          child: Center(
-              child: Container(
-            padding: EdgeInsets.all(10.0),
-            child: Text(
-              _cardText,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontFamily: 'OtomanopeeOne-Regular',
-                fontSize: screen_width / 15,
-                color: Colors.white,
+    return Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: () {
+            print('hello');
+          },
+          child: Card(
+            elevation: 50,
+            shape: RoundedRectangleBorder(
+              side: BorderSide(
+                color: Colors.black,
+                width: screen_width / 150,
               ),
+              borderRadius: BorderRadius.circular(50),
             ),
-          ))),
-      color: _color,
-    );
+            child: SizedBox(
+                width: screen_width / 2.8,
+                height: screen_height / 5.9,
+                child: Center(
+                    child: Container(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    _cardText,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'OtomanopeeOne-Regular',
+                      fontSize: screen_width / 15,
+                      color: Colors.white,
+                    ),
+                  ),
+                ))),
+            color: _color,
+          ),
+        ));
   }
 }
