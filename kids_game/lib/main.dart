0cd7a0850fcc6204e9fd04bf958c0f37d7e455ac
Jawed Alam <zjaweds@outlook.com>
2021-08-23 00:42:53 +0530

    Deleted unnecessary files and removed unnecessary code from counting screen and route generator files

--- kids_game/lib/main.dart
+++ kids_game/lib/main.dart
@@ -2,20 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:kids_game/route_generator.dart';
 import 'HomeScreen/home_screen.dart';
 
-// import 'HomeScreen/background.dart';
-// import 'Games/counting_screen.dart';
-
-// import 'Games/orientation.dart';
-// import 'Games/games_background.dart';
-// import 'Games/matching_color.dart';
-// import 'Games/odd_one_out.dart';
-// import 'Games/phonics.dart';
-
-// void main() => runApp(const MaterialApp(
-//       home: HomeScreen(),
-//       debugShowCheckedModeBanner: false,
-//     ));
-
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -24,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      //Home must be there in order to provide navigation capability
       home: HomeScreen(),
       debugShowCheckedModeBanner: false,
       // Initially display FirstPage
@@ -32,52 +19,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// class LandingScreen extends StatelessWidget {
-//   const LandingScreen({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       home: HomeScreen(),
-//       debugShowCheckedModeBanner: false,
-//     );
-//   }
-// }
-// void main() => runApp(const MaterialApp(
-//       home: ProperOrientation(),
-//       debugShowCheckedModeBanner: false,
-//     ));
-
-
-
-// void main() {
-//   runApp(MaterialApp(home: Background()));
-// }
-// void main() {
-//   runApp(const MaterialApp(
-//     home: OddOneOut(),
-//     debugShowCheckedModeBanner: false,
-//   ));
-// }
-
-// void main() {
-//   runApp(MaterialApp(
-//     home: MatchingColors(),
-//     debugShowCheckedModeBanner: false,
-//   ));
-// }
-
-// void main() {
-//   runApp(MaterialApp(home: Foreground()));
-// }
-
-// void main() => runApp(const MaterialApp(
-//       home: CountingScreen(),
-//       debugShowCheckedModeBanner: false,
-//     ));
-
-// void main() => runApp(const MaterialApp(
-//       home: Phonics(),
-//       debugShowCheckedModeBanner: false,
-//     ));
