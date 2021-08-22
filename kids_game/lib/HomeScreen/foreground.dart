dd670fd67eb1e1779093b9f670dd685100194e6f
Jawed Alam <zjaweds@outlook.com>
2021-08-23 05:14:36 +0530

    Owls postion adjusted

--- kids_game/lib/HomeScreen/foreground.dart
+++ kids_game/lib/HomeScreen/foreground.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 
 class Foreground extends StatelessWidget {
+  const Foreground({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Column(
+    return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Material(
           type: MaterialType.transparency,
           child: Container(
-            padding: EdgeInsets.only(top: 10.0, left: 10.0),
-            child: Text(
+            padding: const EdgeInsets.only(top: 10.0, left: 20.0),
+            child: const Text(
               "Brainiacs",
               style: TextStyle(
                 fontFamily: 'OtomanopeeOne-Regular',
@@ -22,25 +23,25 @@ class Foreground extends StatelessWidget {
           ),
         ),
         Container(
-          padding: EdgeInsets.only(left: 100.0, bottom: 60.0),
+          padding: const EdgeInsets.only(left: 100.0, bottom: 70.0),
           child: Image.asset(
             'assets/images/owls.png',
           ),
         ),
         Container(
-          padding: EdgeInsets.only(left: 200.0, bottom: 50.0),
+          padding: const EdgeInsets.only(left: 200.0, bottom: 50.0),
           child: Image.asset(
             'assets/images/bird.png',
           ),
         ),
         Container(
-          padding: EdgeInsets.only(left: 20.0, top: 10.0),
+          padding: const EdgeInsets.only(left: 20.0, top: 10.0),
           child: Image.asset(
             'assets/images/monkey.png',
           ),
         ),
         Container(
-          padding: EdgeInsets.only(
+          padding: const EdgeInsets.only(
             top: 60.0,
           ),
           child: Image.asset(
@@ -48,6 +49,6 @@ class Foreground extends StatelessWidget {
           ),
         ),
       ],
-    ));
+    );
   }
 }
